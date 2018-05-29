3f3ea1b1f6a0128dcc20612c8601548abf9df0e6
Edi Carlos note <edi_csilva@hotmail.com>
2018-05-29 16:59:47 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+// declara o express
+var express = require('express');
+
+// carrega o express e atribui à variavel app
+// variável app é a referencia para o objeto do express
+var app = express();
+
+app.listen(3000, function() {
+    console.log("Servidor rodando na porta 3000!");
+});
+
+// definindo uma rota de teste... localhost:3000/teste
+app.get('/teste', function(req, res) {
+    console.log('Recebida requisiçao de teste na porta 3000.');
+    res.send('Ok.');
+});
