eaf79972d8bc86acf1579b8892211f8d1ae3a138
Edi Carlos note <edi_csilva@hotmail.com>
2018-05-30 22:53:44 -0300

    primeiras requisições post

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,9 +1,13 @@
-
-
 module.exports = function(app) {
     // definindo uma rota de teste... localhost:3000/pagamentos
     app.get('/pagamentos', function(req, res) {
         console.log('Recebida requisiçao de teste na porta 3000.');
         res.send('Ok.');
+    });
+
+    app.post('/pagamentos/pagamento', function(req, res) {
+        var pagamento = req.body;
+        console.log(pagamento);
+        res.send('ok');
     });
 }
