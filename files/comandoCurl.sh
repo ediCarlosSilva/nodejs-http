c39b71fbd398201ee3ae187d29fe89bf60031e72
Edi Carlos note <edi_csilva@hotmail.com>
2018-06-05 11:19:42 -0300

    commando curl

--- files/comandoCurl.sh
+++ files/comandoCurl.sh
@@ -3,8 +3,18 @@ curl http://localhost:3000/pagamentos/pagamento -X POST -v -H "Content-type: app
     "valor": 10.98,
     "moeda": "BRL",
     "descricao": "criando um pagamento"
-}'; echo
+}'
 
 # curl com entrada através de um arquivo
 # pagamento.json precisa estár localizado na mesma pasta que o comando está sendo executado.
-curl http://localhost:3000/pagamentos/pagamento -X POST -v -H "Content-type: application/json" -d @pagamento.json; echo
+curl http://localhost:3000/pagamentos/pagamento -X POST
+ -v -H "Content-type: application/json" -d @files/pagamento.json; echo
+
+ # curl com entrada através de um arquivo e um pipe utilizando json_pp para formatar a saida
+# pagamento.json precisa estár localizado na mesma pasta que o comando está sendo executado.
+curl http://localhost:3000/pagamentos/pagamento -X POST
+ -v -H "Content-type: application/json" -d @files/pagamento.json | json_pp
+
+# comando curl no windows com cmder
+# não precisa do -X POST, mas se colocar só da warn
+curl http://localhost:3000/pagamentos/pagamento -v -H "Content-type: application/json" -d -@files/pagamento.json
