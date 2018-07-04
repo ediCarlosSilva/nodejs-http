1afabbae63d253192d78789b4f166a915db84a92
Edi Carlos note <edi_csilva@hotmail.com>
2018-07-04 16:53:56 -0300

    implementado gerenciamento de thread

--- cluster.js
+++ cluster.js
@@ -1,9 +1,33 @@
 var cluster = require('cluster');
 var os = require('os');
 
+console.log('executando thread.');
+
 const cpus = os.cpus();
-console.log(cpus);
+// console.log(cpus);
 
-// cpus.forEach(function() {
+if (cluster.isMaster) {
+    console.log('thread master');
 
-// })
+    cpus.forEach(function() {
+        console.log('dentro do forEach');
+        cluster.fork();
+    });
+
+    cluster.on('listening', worker => {
+        console.log('cluster %d conectado', worker.process.pid);
+    });
+
+    cluster.on('disconnect', worker => {
+        console.log('cluster %d desconectado', worker.process.pid);
+    });
+
+    cluster.on('exit', worker => {
+        console.log('cluster %d perdido', worker.process.pid);
+        cluster.fork();
+    })
+
+} else {
+    console.log('thread slave.');
+    require('./index.js');
+}
